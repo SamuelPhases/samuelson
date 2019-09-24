1d74f6316a3750cef54d850e0a568ee6cab6fc36
Denis John <denis.john.ndarake@gmail.com>
2019-09-24 14:50:07 +0100

    Added some basic configurations

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('pagination', 'xmlrpc' , 'form_validation', 'email','upload','encryption','database','session');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url','file','form','security','string','inflector','directory','download','date');
 
 /*
 | -------------------------------------------------------------------
